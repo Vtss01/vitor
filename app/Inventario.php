438c3ae705c8981841dd9810038b0f861882492d
Vitor <vitorgss01@gmail.com>
2019-10-22 18:07:07 -0400

    Criando migracao de inventario

---
+++ app/Inventario.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Inventario extends Model
+{
+    protected $table = 'inventarios';
+
+    protected $fillable =[
+
+    ]
+
+    private
+
+    public
+}
